45d54856673c770a307c9e92e9a7bb32974f47b2
Chrisdocs <chrisa.wolfe@gmail.com>
2021-08-18 11:12:00 -0500

    installed react-carousel and started bike component

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Bike from '../components/Bike';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css'
+import '../assets/styles/dashboard.css';
 
 const Dashboard = () => {
+
     return (
         <div className="">
-            This is the Dashboard
+            <Bike />
         </div>
     )
 }
