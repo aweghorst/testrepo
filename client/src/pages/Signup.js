a8d49376640a8479c2e25580837f4584dbc435c4
dhartley <dani_hartley@msn.com>
2021-08-18 19:43:59 -0500

    added signup form and login form

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,11 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useMutation } from '@apollo/react-hooks';
+import { ADD_USER } from '../utils/mutations';
+
+import Auth from '../utils/auth';
 
 const Signup = () => {
+    const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+    const [addUser, { error }] = useMutation(ADD_USER);
+
+    // update state based on form input changes
+    const handleChange = event => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+    };
+
+    // submit form
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+
+        try {
+            const { data } = await addUser({
+                variables: { ...formState }
+            });
+
+            Auth.login(data.addUser.token);
+        } catch (e) {
+            console.error(e);
+        }
+    };
+
     return (
         <div className="">
-            this is the signup page
+            <div className="flex p-20 justify-center">
+                <div className="w-full max-w-xs">
+                    <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleFormSubmit}>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                                Username
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus: outline-none focus: shadow-outline" id="username" type="text" placeholder="Username" value={formState.username}
+                                onChange={handleChange} />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                                Email
+                            </label>
+                            <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus: outline-none focus: shadow-outline" id="email" type="text" placeholder="Email" value={formState.email}
+                                onChange={handleChange} />
+                        </div>
+                        <div className="mb-6">
+                            <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
+                                Password
+                            </label>
+                            <input className="shadow appearance-none border border-read-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus: shadow-outline" id="password" type="password" placeholder="***********" value={formState.email}
+                                onChange={handleChange} />
+                            <p className="text-red-500 text-xs italic">Please choose a password.</p>
+                        </div>
+                        <div className="flex items-center justify-between">
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus: shadow-outline" type="button">
+                                Sign Up
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-    )
-}
+    );
+};
 
 export default Signup;
