f36bbd286a74ff0025f409b12ce6b61aff157962
dhartley <dani_hartley@msn.com>
2021-08-20 17:53:43 -0500

    change and submit handle

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -6,8 +6,39 @@ import { LOGIN } from '../../utils/mutations';
 
 import Auth from '../../utils/auth';
 
-const LoginForm = () => {
-    
+const LoginForm = props => {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [login, { error }] = useMutation(LOGIN);
+
+    //update state based on form input changes 
+    const handleChange = event => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+    };
+
+    //submit form
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+
+        try {
+            const { data } = await login({
+                variables: { ...formState }
+            });
+            Auth.login(data.login.token);
+        } catch (e) {
+            console.error(e);
+        }
+
+        // clear form values
+        setFormState({
+            email: '',
+            password: '',
+        });
+    };
 
     return (
         <div className="flex p-20 justify-center">
