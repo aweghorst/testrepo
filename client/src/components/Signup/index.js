0543ab56c883ea70bf3c0fcea10228ae775c3d89
dhartley <dani_hartley@msn.com>
2021-08-20 18:01:47 -0500

    added change and submit handles

--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -5,6 +5,33 @@ import { ADD_USER } from '../../utils/mutations';
 import Auth from '../../utils/auth';
 
 const SignupForm = () => {
+    const [formState, setFormState] = useState({ username: '', email: '', password: '' });
+    const [addUser] = useMutation(ADD_USER);
+
+    // update state based on form input changes
+    const handleChange = event => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+    };
+
+    // submit form
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+
+        try {
+            const { data } = await addUser({
+                variables: { ...formState }
+            });
+
+            Auth.login(data.addUser.token);
+        } catch (e) {
+            console.error(e);
+        }
+    };
     return (
         <div className="">
             <div className="flex p-20 justify-center">
