c31c68e9b34d6a998537b88c5c0c73269a24076c
Chrisdocs <chrisa.wolfe@gmail.com>
2021-08-20 11:25:28 -0500

    added BikeMessage Component

---
+++ client/src/components/BikeMessage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const BikeMessage = () => {
+
+    return (
+        <div>
+            <div>
+            <p>From: User</p>
+            <p>Sent on: Monday</p>
+            </div>
+            <p>Message Text</p>
+        </div>
+    )
+}
--- client/src/components/UserBike/index.js
+++ client/src/components/UserBike/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css'
 import bike from '../../assets/images/bike.jpg';
-import BikeComment from '../BikeComment';
+import BikeMessage from '../BikeMessage';
 
 const UserBike = () => {
 
