cac4bd32c0c3d40d0cd3ba3ecbf9890d9b36a294
dhartley <dani_hartley@msn.com>
2021-08-20 18:12:58 -0500

    updated layout

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -58,8 +58,12 @@ const LoginForm = props => {
                         <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                             Password
                         </label>
-                        <input className="focus:outline-none focus:ring focus:border-blue-300 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="*********" value={formState.password}
-                            onChange={handleChange} />
+                        <input className="focus:outline-none focus:ring focus:border-blue-300 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
+                        id="password" 
+                        type="password" 
+                        placeholder="*********" 
+                        value={formState.password}
+                        onChange={handleChange} />
                     </div>
                     <div className="flex items-center justify-between">
                         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus: shadow-outline" type="button">
--- client/src/components/Signup/index.js
+++ client/src/components/Signup/index.js
@@ -41,22 +41,34 @@ const SignupForm = () => {
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
                                 Username
                             </label>
-                            <input className="focus:outline-none focus:ring focus:border-blue-300 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" value={formState.username}
-                                onChange={handleChange} />
+                            <input className="focus:outline-none focus:ring focus:border-blue-300 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+                            id="username" 
+                            type="text" 
+                            placeholder="Username" 
+                            value={formState.username}
+                            onChange={handleChange} />
                         </div>
                         <div className="mb-4">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
                                 Email
                             </label>
-                            <input className="focus:outline-none focus:ring focus:border-blue-300 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="Email" value={formState.email}
-                                onChange={handleChange} />
+                            <input className="focus:outline-none focus:ring focus:border-blue-300 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
+                            id="email" 
+                            type="text" 
+                            placeholder="Email" 
+                            value={formState.email}
+                            onChange={handleChange} />
                         </div>
                         <div className="mb-6">
                             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
                                 Password
                             </label>
-                            <input className="focus:outline-none focus:ring focus:border-blue-300 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="***********" value={formState.email}
-                                onChange={handleChange} />
+                            <input className="focus:outline-none focus:ring focus:border-blue-300 shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" 
+                            id="password" 
+                            type="password" 
+                            placeholder="***********" 
+                            value={formState.email}
+                            onChange={handleChange} />
                             <p className="text-red-500 text-xs italic">Please choose a password.</p>
                         </div>
                         <div className="flex items-center justify-between">
