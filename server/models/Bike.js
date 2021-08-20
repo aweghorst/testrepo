e177f257dc97a8e921b8626ec0ad6c2e4d9f9eaa
Chrisdocs <chrisa.wolfe@gmail.com>
2021-08-20 17:23:45 -0500

    removed required feild for model and brand

--- server/models/Bike.js
+++ server/models/Bike.js
@@ -8,11 +8,9 @@ const bikeSchema = new Schema(
     {
         brand: {
             type: String,
-            required: true
         },
         bike_model: {
             type: String,
-            required: true
         },
         year: {
             type: String
