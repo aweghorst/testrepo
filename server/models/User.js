990d1744d3cbbd5a08abc5837feabb067914d7b9
Chrisdocs <chrisa.wolfe@gmail.com>
2021-08-20 16:53:24 -0500

    updated server/model/user -> bikes:[ { type: Schema.Types.ObjectId, ref: Bike }]

--- server/models/User.js
+++ server/models/User.js
@@ -21,7 +21,7 @@ const userSchema = new Schema(
       required: true,
       minlength: 5,
     },
-    bikes: [Bike.schema],
+    bikes:[ { type: Schema.Types.ObjectId, ref: "Bike" }]
   },
   {
     toJSON: {
