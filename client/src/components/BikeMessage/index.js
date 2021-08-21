13c25027064e4957b79933deb7c7cffd5def9967
Chrisdocs <chrisa.wolfe@gmail.com>
2021-08-21 14:01:34 -0500

    carousel render and user bike cards

--- client/src/components/BikeMessage/index.js
+++ client/src/components/BikeMessage/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const BikeMessage = ( {messages } ) => {
+const BikeMessage = ({messages}) => {
 
     return (
         <div className="container text-center">
@@ -9,6 +9,8 @@ const BikeMessage = ( {messages } ) => {
                     messages.map(message => (
                         <p className="" key={message._id}>
                         {message.messageBody}
+                        posted by
+                        {message.username} on {message.createdAt}
                         </p>
                     ))}
             </div>
