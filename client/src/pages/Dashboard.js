cac22c62846d2a493e7f762ae19b11367869b2a4
Chrisdocs <chrisa.wolfe@gmail.com>
2021-08-23 10:04:41 -0500

    push @10AM 823

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -19,7 +19,7 @@ const Dashboard = () => {
   return (
     <div className="w-screen">
       {Auth.loggedIn() ? (
-      <div className=" flex flex-col items-center">
+      <div className=" flex flex-col items-center pt-20">
           <div className="username">Hello {username}!</div>
           <div className="welcome">Here you can upload your bike information and if a bike is marked as missing, it will be automatically added to the Search page!</div>
       </div>
