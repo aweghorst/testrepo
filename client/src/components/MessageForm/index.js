568e55b1c325763c7ee2b369fa516a57f47b7868
Chrisdocs <chrisa.wolfe@gmail.com>
2021-08-23 17:15:23 -0500

    messages send correctly

--- client/src/components/MessageForm/index.js
+++ client/src/components/MessageForm/index.js
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_MESSAGE } from '../../utils/mutations';
+import { Redirect } from 'react-router-dom';
 
-const MessageForm = (bikeId) => {
+const MessageForm = () => {
 
-    const [ messageBody, setText] = useState('');
+    const [ messageBody, setMessageBody] = useState('');
     const [ addMessage, { error }] = useMutation(ADD_MESSAGE);
+    const [ bikeId, setBikeId ] = useState('');
+
+    let currentIdUrl = window.location.hash.split('/').splice(2).toString();
 
     function handleChange(event) {
-        setText(event.target.value);
+        setMessageBody(event.target.value);
+        setBikeId(currentIdUrl);
     }
 
     async function handleFormSubmit(event) {
@@ -19,11 +24,14 @@ const MessageForm = (bikeId) => {
                 variables: { bikeId, messageBody }
             });
 
-            setText('');
+            setMessageBody('');
+            setBikeId('');
+
         }
         catch (e) {
             console.error(e);
         }
+
     };
 
     return (
@@ -32,7 +40,7 @@ const MessageForm = (bikeId) => {
                 <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" onSubmit={handleFormSubmit}>
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="comment">Let user know what you know!</label>
                     <textarea className="border rounded-md outline" rows="8" cols="30" placeholder="type your message here!" onChange={handleChange}></textarea>
-                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">Submit</button>
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" >Submit</button>
                 </form>
             </div>
         </div>
