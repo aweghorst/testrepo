f8e795b3893ae5e47b63da2e48642523b53a02ec
dhartley <dani_hartley@msn.com>
2021-08-24 20:11:42 -0500

    added how it works to homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,11 +12,20 @@ const Home = () => {
                 <p className="welcome-text dark:text-gray-200">BikeSleuth is designed to quickly reunite a rider and their missing bicycle. With this app, you are able to upload your bicycle information at any time and in the event the bike becomes lost or stolen, you can update your bike's status as missing and your bike will be automatically added to our search page allowing other users in your area to keep an eye out for your missing bicycle. </p>
             </div>
             <div>
-                <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-6xl space-x-3">
+            <h4 className="how-to"><span className="dark:text-gray-200">How Does It Work!</span></h4>
+            </div>
+            <div>
+                <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-4xl">
                     <div class="md:flex">
-                        <div class="p-8">
-                            <p class="block mt-1 text-lg leading-tight font-medium text-black underline">Double up your security by using two high-quality locks</p>
-                            <p class="mt-2 text-gray-500">U-locks tend to be more durable than thin cable locks, which can be cut through more easily. But because of their shape, U-locks limit the amount of bike you can secure at one time, so consider pairing yours with a sturdy steel chain.</p>
+                        <div className="how-to-list" class="p-8">
+                            <p class="block mt-1 text-lg leading-tight font-medium text-black underline">Register Your Bike</p>
+                            <p class="mt-2 text-gray-500">It’s simple. Sign up and register your bike(s). Enter the brand, model, year, serial number, description and your city.</p>
+                            <p class="block mt-1 text-lg leading-tight font-medium text-black underline">Update Your Bikes Status</p>
+                            <p class="mt-2 text-gray-500">If your bike goes missing, update it status to missing. It will then be added to the list of missing bikes in the community. Notifying your fellow bikeslueths to keep an eye out for it. </p>
+                            <p class="block mt-1 text-lg leading-tight font-medium text-black underline">Share Information</p>
+                            <p class="mt-2 text-gray-500">If a fellow bikeslueth spots or finds your bike they will be able to message personally.</p>
+                            <p class="block mt-1 text-lg leading-tight font-medium text-black underline">You Get Your Bike Back</p>
+                            <p class="mt-2 text-gray-500">With the help of your fellow bikeslueth you have the information necessary to recover your missing bike.</p>
                         </div>
                     </div>
                 </div>
